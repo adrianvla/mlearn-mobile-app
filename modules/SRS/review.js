2afd52f5882c0535bdad17d6cf8864a249a1bbbf
adrianvla <admin@morisinc.net>
2025-08-16 19:37:41 +0200

    Add edit and create modes for flashcards with dynamic UI updates

--- modules/SRS/review.js
+++ modules/SRS/review.js
@@ -153,8 +153,11 @@ export const review = () => {
         fs.flashcards.shift();
         displayLast();
     }
+    let isInEditMode = false;
+    let isInCreateMode = false;
     $(document).on('keydown', (e) => {
         console.log(e.key);
+        if(isInEditMode || isInCreateMode) return;
         switch(e.key){
             case "1":
                 $(".btn.again").click();
@@ -193,7 +196,6 @@ export const review = () => {
     });
     $(".btn.bin").on('click',()=>removeFlashcard());
     $(".btn.close").on('click',displayHomeScreen);
-    let isInEditMode = false;
     $(".editMode").hide();
     $(".btn.edit").on('click',()=>{
         if(isInEditMode){
@@ -204,12 +206,83 @@ export const review = () => {
             fs.flashcards[0].content.exampleMeaning = $(".example .translation p").html();
             fs.flashcards[0].content.definition = $(".definition").html();
             fs.flashcards[0].lastUpdated = Date.now();
+            $(".buttons,.btn.add-flashcard").show();
             fs = sortByDueDate(fs);
+            $(".btn.edit").html(`<svg width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="Complete"><g id="edit"><g><path d="M20,16v4a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V6A2,2,0,0,1,4,4H8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/><polygon fill="none" points="12.5 15.8 22 6.2 17.8 2 8.3 11.5 8 16 12.5 15.8" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></g></g></g></svg>`);
         }else{
+            revealAnswer(fs.flashcards[0]);
             $(".can-be-edited").attr("contenteditable", "true");
             $(".editMode").text("Edit Mode").show();
+            $(".buttons,.btn.add-flashcard").hide();
+            $(".btn.edit").html(`<svg width="800px" height="800px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"><g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-152.000000, -515.000000)" fill="currentColor"><path d="M171,525 C171.552,525 172,524.553 172,524 L172,520 C172,519.447 171.552,519 171,519 C170.448,519 170,519.447 170,520 L170,524 C170,524.553 170.448,525 171,525 L171,525 Z M182,543 C182,544.104 181.104,545 180,545 L156,545 C154.896,545 154,544.104 154,543 L154,519 C154,517.896 154.896,517 156,517 L158,517 L158,527 C158,528.104 158.896,529 160,529 L176,529 C177.104,529 178,528.104 178,527 L178,517 L180,517 C181.104,517 182,517.896 182,519 L182,543 L182,543 Z M160,517 L176,517 L176,526 C176,526.553 175.552,527 175,527 L161,527 C160.448,527 160,526.553 160,526 L160,517 L160,517 Z M180,515 L156,515 C153.791,515 152,516.791 152,519 L152,543 C152,545.209 153.791,547 156,547 L180,547 C182.209,547 184,545.209 184,543 L184,519 C184,516.791 182.209,515 180,515 L180,515 Z" id="save-floppy" sketch:type="MSShapeGroup"></path></g></g></svg>`);
         }
         isInEditMode = !isInEditMode;
+    });
+    $(".btn.save").hide();
+    $(".btn.add-flashcard").on('click',()=>{
+        if(isInCreateMode){
+            $(".btn.edit,.buttons").show();
+            $(".editMode,.pronunciation").hide();
+            $(".can-be-edited").attr("contenteditable", "false");
+            $(".card-item:has(.img-src),.pitch").hide();
+            fs.flashcards.unshift({
+                "content":{
+                    "word":$(".word").text(),
+                    "pitchAccent":$(".pitch span").text(),
+                    "pronunciation":$(".pronunciation span").text(),
+                    "translation":$(".translation").text(),
+                    "definition":$(".definition").html(),
+                    "example":$(".sentence").html(),
+                    "exampleMeaning":$(".example .translation p").html(),
+                    "screenshotUrl":$(".img-src").text(),
+                    "pos": $(".pill").text(),
+                    "level": parseInt($(".pill").attr("level")) || -1
+                },
+                "dueDate":Date.now(),
+                "lastReviewed":Date.now(),
+                "lastUpdated":Date.now(),
+                "ease":0,
+                "reviews":0
+            });
+            $(".btn.add-flashcard").html(`<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11 8C11 7.44772 11.4477 7 12 7C12.5523 7 13 7.44772 13 8V11H16C16.5523 11 17 11.4477 17 12C17 12.5523 16.5523 13 16 13H13V16C13 16.5523 12.5523 17 12 17C11.4477 17 11 16.5523 11 16V13H8C7.44771 13 7 12.5523 7 12C7 11.4477 7.44772 11 8 11H11V8Z" fill="currentColor"/><path fill-rule="evenodd" clip-rule="evenodd" d="M23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12ZM3.00683 12C3.00683 16.9668 7.03321 20.9932 12 20.9932C16.9668 20.9932 20.9932 16.9668 20.9932 12C20.9932 7.03321 16.9668 3.00683 12 3.00683C7.03321 3.00683 3.00683 7.03321 3.00683 12Z" fill="currentColor"/></svg>`);
+        }else{
+            $(".btn.edit,.buttons").hide();
+            $(".editMode").text("Add Flashcard").show();
+            $(".can-be-edited").attr("contenteditable", "true");
+            let fs = {
+                "content":{
+                    "word":"word",
+                    "pitchAccent":undefined,
+                    "pronunciation":"pronunciation",
+                    "translation":"translation",
+                    "definition":"definition",
+                    "example":"example",
+                    "exampleMeaning":"example meaning",
+                    "screenshotUrl":"-",
+                    "pos": "",
+                    "level": -1
+                },
+                "dueDate":1755100026393,
+                "lastReviewed":1755100026393,
+                "lastUpdated":1755100026393,
+                "ease":0,
+                "reviews":0
+            };
+            displayFlashcard(fs);
+
+            $(".answer,.pronunciation").show();
+            $(".pronunciation span").text("pronunciation");
+            $(".example .translation p").html(fs.content.exampleMeaning);
+            $(".card-item:has(.definition)").show();
+
+
+            $(".pill").text("Level ID").show();
+            $(".card-item:has(.img-src),.pitch").show();
+            $(".img-src").text("image source");
+            $(".pitch span").text("-1");
+            $(".btn.add-flashcard").html(`<svg width="800px" height="800px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"><g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-152.000000, -515.000000)" fill="currentColor"><path d="M171,525 C171.552,525 172,524.553 172,524 L172,520 C172,519.447 171.552,519 171,519 C170.448,519 170,519.447 170,520 L170,524 C170,524.553 170.448,525 171,525 L171,525 Z M182,543 C182,544.104 181.104,545 180,545 L156,545 C154.896,545 154,544.104 154,543 L154,519 C154,517.896 154.896,517 156,517 L158,517 L158,527 C158,528.104 158.896,529 160,529 L176,529 C177.104,529 178,528.104 178,527 L178,517 L180,517 C181.104,517 182,517.896 182,519 L182,543 L182,543 Z M160,517 L176,517 L176,526 C176,526.553 175.552,527 175,527 L161,527 C160.448,527 160,526.553 160,526 L160,517 L160,517 Z M180,515 L156,515 C153.791,515 152,516.791 152,519 L152,543 C152,545.209 153.791,547 156,547 L180,547 C182.209,547 184,545.209 184,543 L184,519 C184,516.791 182.209,515 180,515 L180,515 Z" id="save-floppy" sketch:type="MSShapeGroup"></path></g></g></svg>`);
+        }
+        isInCreateMode = !isInCreateMode;
     });
 
 };
